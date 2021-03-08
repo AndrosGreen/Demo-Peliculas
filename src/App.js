ab7f9ec30fda5adcd1f98aa7f6c030dd0daeab4e
AndrosGreen <gclandres@gmail.com>
2021-03-08 12:16:44 -0600

    agregar peliculas

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ import MovieDeleteModal from './modals/MovieDeleteModal';
 
 class App extends React.Component {
 
-    state = { movies : [], term:'', show: false, idMovie : 0 };
+    state = { 
+        movies : [], 
+        term: '', 
+        show: false, 
+        idMovie : 0, 
+        movieNombre : '',
+        movieRating : '',
+    };
 
     getPeliculas = async (event) => {
         event.preventDefault();
@@ -15,6 +22,18 @@ class App extends React.Component {
         });
         console.log(response.data);
         this.setState( {movies : response.data} );
+    }
+
+    insertMovie = async (event) => {
+        event.preventDefault();
+        //console.log(this.state.movieNombre);
+        //console.log(this.state.movieRating);
+        const response = await apiPeliculas.post('/add',{
+            nombre : this.state.movieNombre,
+            rating : this.state.movieRating
+        });
+        console.log(response.data);
+        this.loadMovies();
     }
 
     loadMovies = async () => {
@@ -46,17 +65,29 @@ class App extends React.Component {
         return (
           <div className="container" style={{marginTop: "20px"}}>
 
-            <form onSubmit={this.getPeliculas}>
-              <h3>Agregar Pelicula:</h3>
+            <form onSubmit={this.insertMovie}>
+                <h3>Agregar Pelicula:</h3>
+
+                <h5>Nombre</h5>
                 <input
                     className="form-control"
-                    onChange = {(e) => this.setState({term: e.target.value})}
-                    value = {this.state.term}
+                    onChange = {(e) => this.setState( { movieNombre : e.target.value } )}
+                    value = {this.state.movieNombre }
                 />
+
+                <h5>Rating</h5>
+                <input
+                    className="form-control"
+                    onChange = {(e) => this.setState( { movieRating : e.target.value} )}
+                    value = {this.state.movieRating}
+                />
+
                 <button className="btn btn-primary" style={{margin: "5px"}}> Add Movie </button>
             </form>
-            <button className="btn btn-primary" onClick={this.getPeliculas} style={{margin: "5px"}}> Show All </button>
-            <button className="btn btn-primary" onClick={this.handleOpen} style={{margin: "5px"}}> Modal </button>
+            
+            <button className="btn btn-primary" onClick={this.loadMovies} style={{margin: "5px"}}> Show All </button>
+            <button className="btn btn-primary" style={{margin: "5px"}}> Modal </button>
+            
             <MovieList movies = {this.state.movies} handleOpen={this.handleOpen}/>
 
             <MovieDeleteModal 
